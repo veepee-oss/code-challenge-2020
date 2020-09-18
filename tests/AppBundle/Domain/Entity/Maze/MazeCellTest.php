b6c758372b024cb97d1686f861444c34039086dc
David Amigo <david.amigo@privalia.com>
2020-09-18 13:47:12 +0200

    Improve the unit tests of the MazeCell domain entity

--- tests/AppBundle/Domain/Entity/Maze/MazeCellTest.php
+++ tests/AppBundle/Domain/Entity/Maze/MazeCellTest.php
@@ -50,4 +50,69 @@ class MazeCellTest extends TestCase
         $cell = new MazeCell(7);
         $this->assertFalse($cell->isEmpty());
     }
+
+    /**
+     * Test newEmptyCell creates an empty cell
+     */
+    public function testNewEmptyCell()
+    {
+        $cell = MazeCell::newEmptyCell();
+        $this->assertTrue($cell->isEmpty());
+    }
+
+    /**
+     * Test newWallCell creates a wall cell
+     */
+    public function testNewWallCell()
+    {
+        $cell = MazeCell::newWallCell();
+        $this->assertTrue($cell->isWall());
+    }
+
+    /**
+     * Test setEmpty sets the cell empty
+     */
+    public function testSetEmpty()
+    {
+        $cell = new MazeCell(5);
+        $cell->setEmpty();
+        $this->assertTrue($cell->isEmpty());
+    }
+
+    /**
+     * Test setWall sets the cell wall
+     */
+    public function testSetWall()
+    {
+        $cell = new MazeCell(5);
+        $cell->setWall();
+        $this->assertTrue($cell->isWall());
+    }
+
+    /**
+     * Test getWallIndex return an index
+     */
+    public function testGetWallIndex0()
+    {
+        $cell = new MazeCell(0x80);
+        $this->assertEquals(0, $cell->getWallIndex());
+    }
+
+    /**
+     * Test getWallIndex return an index
+     */
+    public function testGetWallIndex1()
+    {
+        $cell = new MazeCell(0x81);
+        $this->assertEquals(1, $cell->getWallIndex());
+    }
+
+    /**
+     * Test getWallIndex return an index
+     */
+    public function testGetWallIndex5()
+    {
+        $cell = new MazeCell(0x85);
+        $this->assertEquals(5, $cell->getWallIndex());
+    }
 }
